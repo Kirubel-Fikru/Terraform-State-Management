042c10f2e4cc35e99436eeead185a18a2edaf537
Kirubel-Fikru <kirubelfikruwsh@gmail.com>
2023-11-17 11:01:20 +0300

    Enable versioning

--- main.tf
+++ main.tf
@@ -1,12 +1,21 @@
 provider "aws" {
- region = "us-east-2"
+    region = "us-east-2"
 }
 
 # create an S3 bucket by using the aws_s3_bucket resource:
 resource "aws_s3_bucket" "terraform_state" {
- bucket = "terraform-up-and-running-state"
- # Prevent accidental deletion of this S3 bucket
- lifecycle {
- prevent_destroy = true
+    bucket = "terraform-key-for-backend"
+    # Prevent accidental deletion of this S3 bucket
+    lifecycle {
+        prevent_destroy = true 
+    }
+}
+
+# Enable versioning so you can see the full revision history of your state files
+resource "aws_s3_bucket_versioning" "enabled" {
+    bucket = aws_s3_bucket.terraform_state.id
+    versioning_configuration {
+    status = "Enabled"
  }
 }
+
