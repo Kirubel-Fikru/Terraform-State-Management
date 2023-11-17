4b8df231f0ad9849a6e2e6e87e6e3984692c5caa
Kirubel-Fikru <kirubelfikruwsh@gmail.com>
2023-11-17 12:03:06 +0300

    ADD Varibales and Outputs

--- main.tf
+++ main.tf
@@ -1,10 +1,21 @@
+terraform {
+  required_version = ">= 1.0.0, < 2.0.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
 provider "aws" {
     region = "us-east-2"
 }
 
 # create an S3 bucket by using the aws_s3_bucket resource:
 resource "aws_s3_bucket" "terraform_state" {
-    bucket_prefix = "terraform-state-key"
+    bucket_prefix = var.Bucket_name
     # Prevent accidental deletion of this S3 bucket
     lifecycle {
         prevent_destroy = true 
@@ -40,7 +51,7 @@ resource "aws_s3_bucket_public_access_block" "public_access" {
 
 # create a DynamoDB table to use for locking
 resource "aws_dynamodb_table" "terraform_locks" {
-    name = "terraform-up-and-running-locks"
+    name = var.DynamoDB_name
     billing_mode = "PAY_PER_REQUEST"
     hash_key = "LockID"
     attribute {
---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "Bucket_name" {
+  description = "The bucket name for the s3 bucket for staging"
+  type = string
+}
+
+variable "DynamoDB_name" {
+    description = "Dynameo DB name"
+    type = string
+}
