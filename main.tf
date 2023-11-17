a9e386559eea7058e6843fca57c9629ed13867f2
Kirubel-Fikru <kirubelfikruwsh@gmail.com>
2023-11-17 11:03:25 +0300

    Enable server-side encryption

--- main.tf
+++ main.tf
@@ -15,7 +15,17 @@ resource "aws_s3_bucket" "terraform_state" {
 resource "aws_s3_bucket_versioning" "enabled" {
     bucket = aws_s3_bucket.terraform_state.id
     versioning_configuration {
-    status = "Enabled"
- }
+        status = "Enabled"
+    }
+}
+
+# Enable server-side encryption by default
+resource "aws_s3_bucket_server_side_encryption_configuration" "default" {
+    bucket = aws_s3_bucket.terraform_state.id
+    rule {
+        apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+        }
+    }
 }
 
