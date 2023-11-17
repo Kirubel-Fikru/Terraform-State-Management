26e69e19a8dedead89717d5f86567b34df1f7af7
Kirubel-Fikru <kirubelfikruwsh@gmail.com>
2023-11-17 11:14:12 +0300

    Create a DynamoDB table to use for locking

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 # create an S3 bucket by using the aws_s3_bucket resource:
 resource "aws_s3_bucket" "terraform_state" {
-    bucket = "terraform-state-for-backend-key"
+    bucket_prefix = "terraform-state-key"
     # Prevent accidental deletion of this S3 bucket
     lifecycle {
         prevent_destroy = true 
@@ -36,4 +36,15 @@ resource "aws_s3_bucket_public_access_block" "public_access" {
     block_public_policy = true
     ignore_public_acls = true
     restrict_public_buckets = true
+}
+
+# create a DynamoDB table to use for locking
+resource "aws_dynamodb_table" "terraform_locks" {
+    name = "terraform-up-and-running-locks"
+    billing_mode = "PAY_PER_REQUEST"
+    hash_key = "LockID"
+    attribute {
+        name = "LockID"
+        type = "S"
+    }
 }
